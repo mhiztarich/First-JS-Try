71baccae693364b13236377f67bb06205886f441
Richmond Esosa <esosarichmond@gmail.com>
2022-06-22 16:40:35 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # First-JS-Try
+
 A JavaScript design that prints out My Name, Height and Country.
+
+# Link to the published page
+
+https://mhiztarich.github.io/First-JS-Try/
---
+++ script.js
@@ -0,0 +1,34 @@
+// MY NAME
+
+let myName = "Richmond Esosa"
+
+// MY HEIGHT
+
+let myHeight = "187cm"
+
+// MY COUNTRY
+
+let myCountry = "Nigeria"
+
+
+// TTRYING TO MAKE EACH OF THEM HAVE A SEPERATE LINE
+
+let firstColumn = "Name: " + myName 
+let secondColumn = "\nHeight: " + myHeight
+let thirdColumn = "\nCountry: " + myCountry
+
+
+// RESULTS
+
+// IF WE'RE EXPECTED TO USE CONSOLE
+
+console.log(firstColumn + secondColumn + thirdColumn);
+
+// AND IF WE'RE EXPECTED TO USE ALERT
+
+alert(firstColumn + secondColumn + thirdColumn)
+
+//THIRDLY, IF WE'RE EXPECTED TO USE DOCUMENT.WRITE
+ 
+document.write(firstColumn + secondColumn + thirdColumn)
+
